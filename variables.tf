503c65723d02e87c36f40cb467f81a6f6350c628
Tanner Watson <tannerwatson@gmail.com>
2021-03-08 05:46:50 +0000

    Integrated resource_group_datas module

--- variables.tf
+++ variables.tf
@@ -68,6 +68,11 @@ variable resource_groups {
   default     = {}
 }
 
+variable resource_group_datas {
+  description = "Resource groups configuration objects"
+  default     = {}
+}
+
 variable subscriptions {
   default = {}
 }

703c6cf0c5a707e3821ea633176fbc5bfd7459af
Tanner Watson <tannerwatson@gmail.com>
2021-04-13 11:02:02 -0400

    Fixed group naming

--- modules/azuread/groups/group.tf
+++ modules/azuread/groups/group.tf
@@ -1,6 +1,6 @@
 resource "azuread_group" "group" {
 
-  display_name            = var.global_settings.passthrough ? format("%s", var.azuread_groups.name) : format("%s%s%s", try(format("%s-", var.global_settings.prefixes.0), ""), var.azuread_groups.name, try(format("-%s", var.global_settings.suffixes.0), ""))
+  display_name            = var.global_settings.passthrough ? format("%s", var.azuread_groups.name) : join("-", compact(concat(try(var.global_settings.prefixes, []), [var.azuread_groups.name], try(var.global_settings.suffixes, []))))
   description             = lookup(var.azuread_groups, "description", null)
   prevent_duplicate_names = lookup(var.azuread_groups, "prevent_duplicate_names", null)
 

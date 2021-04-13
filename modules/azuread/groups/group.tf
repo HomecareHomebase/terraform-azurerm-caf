cdbc92045dd081ff00a462675f26b807d067ec19
Tanner Watson <tannerwatson@gmail.com>
2021-04-13 10:38:10 -0400

    groups: Fixed display_name

--- modules/azuread/groups/group.tf
+++ modules/azuread/groups/group.tf
@@ -1,12 +1,7 @@
 resource "azuread_group" "group" {
 
-  display_name             = var.global_settings.passthrough ? format("%s", var.azuread_groups.name) : join("-", compact([local.prefix, var.azuread_groups.name, local.suffix]))
+  display_name            = var.global_settings.passthrough ? format("%s", var.azuread_groups.name) : format("%s%s%s", try(format("%s-", var.global_settings.prefixes.0), ""), var.azuread_groups.name, try(format("-%s", var.global_settings.suffixes.0), ""))
   description             = lookup(var.azuread_groups, "description", null)
   prevent_duplicate_names = lookup(var.azuread_groups, "prevent_duplicate_names", null)
 
-}
-
-locals {
-  prefix = try(var.global_settings.prefix, null) == null ? "" : var.global_settings.prefix.0
-  suffix = try(var.global_settings.suffix, null) == null ? "" : var.global_settings.suffix.0
 }

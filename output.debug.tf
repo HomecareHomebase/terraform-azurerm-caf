3c4bc41173ab3c158cf27b5ad332e9c29e2714ca
Tanner Watson <tannerwatson@gmail.com>
2021-04-25 20:49:54 -0400

    debug

--- output.debug.tf
+++ output.debug.tf
@@ -1,18 +1,28 @@
-output debug {
-    value = tomap({
-        "local_services_roles" = try(local.services_roles, {}),
-        "local_combined_objects_managed_identities" = try(local.combined_objects_managed_identities, {})
-        # "custom_role_definitions" = module.custom_roles,
-        # "roles_to_process" = local.roles_to_process,
-        # "combined_objects_managed_identities" = local.combined_objects_managed_identities
-        # "idents" = [
-        #             for cluster in local.compute.aks_clusters:
-        #             try(local.combined_objects_managed_identities[cluster.value.user_assigned_identity_lz_key][cluster.value.user_assigned_identity_managed_identity_key], {})
-        #             ]
-        # "azuread_groups_members_local_managed_identities_to_process" = module.azuread_groups_members
-        # "combined_objects_azure_container_registries" = local.combined_objects_azure_container_registries
-    })
+# output debug {
+#     value = tomap({
+#         "local_services_roles" = try(local.services_roles, {}),
+#         "local_combined_objects_managed_identities" = try(local.combined_objects_managed_identities, {})
+#         # "custom_role_definitions" = module.custom_roles,
+#         # "roles_to_process" = local.roles_to_process,
+#         # "combined_objects_managed_identities" = local.combined_objects_managed_identities
+#         # "idents" = [
+#         #             for cluster in local.compute.aks_clusters:
+#         #             try(local.combined_objects_managed_identities[cluster.value.user_assigned_identity_lz_key][cluster.value.user_assigned_identity_managed_identity_key], {})
+#         #             ]
+#         # "azuread_groups_members_local_managed_identities_to_process" = module.azuread_groups_members
+#         # "combined_objects_azure_container_registries" = local.combined_objects_azure_container_registries
+#     })
 
+#     sensitive = true
+# }
+
+output debug_local_services_roles {
+    value = local.services_roles
+    sensitive = true
+}
+
+output debug_local_combined_objects_managed_identities {
+    value = local.combined_objects_managed_identities
     sensitive = true
 }
 

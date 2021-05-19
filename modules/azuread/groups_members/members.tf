04a799879df1085021842a66a079442dba2e7048
Tanner Watson <tannerwatson@gmail.com>
2021-05-19 12:04:27 +0000

    cleaned up members

--- modules/azuread/groups_members/members.tf
+++ modules/azuread/groups_members/members.tf
@@ -60,58 +60,58 @@ module managed_identity_keys {
   member_object_id = var.managed_identities[each.key].id
 }
 
-locals {
-  managed_identities_to_process = {
-      for mapping in
-      flatten(
-        [                                                                 # Variable
-          for lz_key, all_mi_mapping in try(var.settings.members.managed_identity_keys, {}) : [          #  built_in_role_mapping = {
-            for mi_key, mi_mapping in all_mi_mapping :
-            {
-              lz_key = lz_key
-              mi_key = mi_key
-              mi_id = var.managed_identities[lz_key][mi_key].id
-              group_id = var.group_id
-            }
-          ]
-        ]
-      ) : format("%s_%s_%s", mapping.group_id, mapping.lz_key, mapping.mi_key) => mapping
-    }
+# locals {
+#   managed_identities_to_process = {
+#       for mapping in
+#       flatten(
+#         [                                                                 # Variable
+#           for lz_key, all_mi_mapping in try(var.settings.members.managed_identity_keys, {}) : [          #  built_in_role_mapping = {
+#             for mi_key, mi_mapping in all_mi_mapping :
+#             {
+#               lz_key = lz_key
+#               mi_key = mi_key
+#               mi_id = var.managed_identities[lz_key][mi_key].id
+#               group_id = var.group_id
+#             }
+#           ]
+#         ]
+#       ) : format("%s_%s_%s", mapping.group_id, mapping.lz_key, mapping.mi_key) => mapping
+#     }
 
 
-  # managed_identities_to_process = {
-  #   for mapping in
-  #    flatten(
-  #     [                                                                 # Variable
-  #       for key_mode, all_role_mapping in var.role_mapping : [          #  built_in_role_mapping = {
-  #         for key, role_mappings in all_role_mapping : [                #       aks_clusters = {
-  #           for scope_key_resource, role_mapping in role_mappings : [   #         seacluster = {
-  #             for role_definition_name, resources in role_mapping : [   #           "Azure Kubernetes Service Cluster Admin Role" = {
-  #               for object_id_key, object_resources in resources : [    #             azuread_group_keys = {
-  #                 for object_id_key_resource in object_resources.keys : #               keys = [ "aks_admins" ] ----End of variable
-  #                 {                                                     # "seacluster_Azure_Kubernetes_Service_Cluster_Admin_Role_aks_admins" = {
-  #                   mode                    = key_mode                  #   "mode" = "built_in_role_mapping"
-  #                   scope_resource_key      = key
-  #                   scope_key_resource      = scope_key_resource
-  #                   role_definition_name    = role_definition_name
-  #                   object_id_resource_type = object_id_key
-  #                   object_id_key_resource  = object_id_key_resource #   "object_id_key_resource" = "aks_admins"
-  #                   lz_key                  = try(object_resources.lz_key, null)
-  #                 }
-  #               ]
-  #             ]
-  #           ]
-  #         ]
-  #       ]
-  #     ]
-  #   ) : format("%s_%s_%s", mapping.scope_key_resource, replace(mapping.role_definition_name, " ", "_"), mapping.object_id_key_resource) => mapping
-  # }
-}
+#   # managed_identities_to_process = {
+#   #   for mapping in
+#   #    flatten(
+#   #     [                                                                 # Variable
+#   #       for key_mode, all_role_mapping in var.role_mapping : [          #  built_in_role_mapping = {
+#   #         for key, role_mappings in all_role_mapping : [                #       aks_clusters = {
+#   #           for scope_key_resource, role_mapping in role_mappings : [   #         seacluster = {
+#   #             for role_definition_name, resources in role_mapping : [   #           "Azure Kubernetes Service Cluster Admin Role" = {
+#   #               for object_id_key, object_resources in resources : [    #             azuread_group_keys = {
+#   #                 for object_id_key_resource in object_resources.keys : #               keys = [ "aks_admins" ] ----End of variable
+#   #                 {                                                     # "seacluster_Azure_Kubernetes_Service_Cluster_Admin_Role_aks_admins" = {
+#   #                   mode                    = key_mode                  #   "mode" = "built_in_role_mapping"
+#   #                   scope_resource_key      = key
+#   #                   scope_key_resource      = scope_key_resource
+#   #                   role_definition_name    = role_definition_name
+#   #                   object_id_resource_type = object_id_key
+#   #                   object_id_key_resource  = object_id_key_resource #   "object_id_key_resource" = "aks_admins"
+#   #                   lz_key                  = try(object_resources.lz_key, null)
+#   #                 }
+#   #               ]
+#   #             ]
+#   #           ]
+#   #         ]
+#   #       ]
+#   #     ]
+#   #   ) : format("%s_%s_%s", mapping.scope_key_resource, replace(mapping.role_definition_name, " ", "_"), mapping.object_id_key_resource) => mapping
+#   # }
+# }
 
-output debug_local_managed_identities_to_process {
-  value = {
-    managed_identities_to_process = local.managed_identities_to_process
-    managed_identities = var.managed_identities
-    members_managed_identity_keys = try(var.settings.members.managed_identity_keys, {})
-  }
-}
+# output debug_local_managed_identities_to_process {
+#   value = {
+#     managed_identities_to_process = local.managed_identities_to_process
+#     managed_identities = var.managed_identities
+#     members_managed_identity_keys = try(var.settings.members.managed_identity_keys, {})
+#   }
+# }

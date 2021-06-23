04e2bb1a6c65545b00aba44b24ed6cac0594ebb5
Tanner Watson <tannerwatson@gmail.com>
2021-06-23 08:11:53 +0000

    scale set boot

--- scale_sets.tf
+++ scale_sets.tf
@@ -18,7 +18,11 @@ module scale_sets {
   # application_gateways             = local.combined_objects_application_gateways
   application_security_groups      = local.combined_objects_application_security_groups
   base_tags                        = try(local.global_settings.inherit_tags, false) ? module.resource_groups[each.value.resource_group_key].tags : {}
-  boot_diagnostics_storage_account = try(local.combined_diagnostics.storage_accounts[each.value.boot_diagnostics_storage_account_key].primary_blob_endpoint, {})
+  # boot_diagnostics_storage_account = try(local.combined_diagnostics.storage_accounts[each.value.boot_diagnostics_storage_account_key].primary_blob_endpoint, {})
+  boot_diagnostics_storage_account = try(local.combined_diagnostics.storage_accounts[each.value.boot_diagnostics_storage_account_key].primary_blob_endpoint,
+    each.value.boot_diagnostics_storage_account_key == "" ? "" : each.value.throw_error,
+  can(tostring(each.value.boot_diagnostics_storage_account_key)) ? each.value.throw_error : null)
+  
   client_config                    = local.client_config
   diagnostics                      = local.combined_diagnostics
   disk_encryption_sets             = local.combined_objects_disk_encryption_sets
